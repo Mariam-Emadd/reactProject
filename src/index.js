383a8d212ec9dbf53baa4a347426fe010979e02b
Mariam-Emadd <elbadahymarium@gmail.com>
2024-06-21 16:18:49 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
-import UserProvider from './website/context/context';
+// import UserProvider from './website/context/context';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { HashRouter } from 'react-router-dom';
 
 // Render the App component into the root element
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider>
+    <HashRouter>
     <App />
-    </UserProvider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
